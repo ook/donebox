02ebf915b3b9719b6bfa9cb3ae6dc639b4cd1c38
macournoyer <macournoyer@ab7993de-5426-0410-a80b-baa00616f331>
2007-10-19 02:46:12 +0000

    testing deployment strategy
    git-svn-id: http://code.macournoyer.com/svn/donebox/trunk@271 ab7993de-5426-0410-a80b-baa00616f331

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,8 @@ role :web, application
 role :app, application
 role :db,  application, :primary => true
 
+set :deploy_via, :remote_cache
 set :deploy_to, "/home/macournoyer/#{application}"
+
 set :use_sudo, false
-set :checkout, "export"
 set :user, 'macournoyer'
