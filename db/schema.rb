420f28378c5894d98e506b86236e1e425fbdaeb6
ook? ook! <tompouce@Tsuki.lecavelier.name>
2008-08-02 21:32:26 +0200

    Remove some non-blocking deprecated feature, set active_record as session method (secret is too ugly...)

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 6) do
+ActiveRecord::Schema.define(:version => 20080802192105) do
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
