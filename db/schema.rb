b00908399e11eb46b6f13f6f24dff4d86fbdf781
Thomas LECAVELIER <thomas@lecavelier.name>
2008-10-14 10:20:01 +0200

    Ignore netbeans directory update db/schema.rb Remove all logs

--- db/schema.rb
+++ db/schema.rb
@@ -9,26 +9,33 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080802192105) do
+ActiveRecord::Schema.define(:version => 20081004184825) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sessions", :force => true do |t|
-    t.string   "session_id", :default => "", :null => false
+    t.string   "session_id", :null => false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "due_on"
     t.datetime "completed_at"
-    t.integer  "position",     :limit => 11
-    t.integer  "user_id",      :limit => 11
-    t.string   "category"
+    t.integer  "position"
+    t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   create_table "users", :force => true do |t|
